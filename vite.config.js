ab72bc7529874611bbcb027250bc3549993f5843
Shaik Mahaboob Razikh <skrazikh1811@gmail.com>
2025-10-17 15:28:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Important for correct routing on Vercel
   plugins: [react()],
-  server: {
-    // optional: allows network access during local dev
-    host: true,
-  }
 })
 
